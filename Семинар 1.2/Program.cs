5b6a2fbf31a51f800af1d8a6b6b7c48503ce093f
Aleksandr <lexx-j@mail.ru>
2023-08-06 00:47:38 +0300

    Домашнее задание

--- Семинар 1.2/Program.cs
+++ Семинар 1.2/Program.cs
@@ -7,9 +7,55 @@
 // 	782 -> 2
 // 	918 -> 8
 
-Console.WriteLine("Введите целое число");
+
+// Console.WriteLine("Введите целое число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num1 = -num;
+// if (num < num1)
+// {
+//     num = num1;
+//     num1 = -num;
+// }
+// while (num1 < num)
+// {
+//     Console.Write($"{num1}, ");
+//     num1++;
+// }
+// Console.Write(num);
+
+
+
+
+// Console.Write("Введите трехзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// while (num > 1000, num < 100)
+// {
+//     Console.Write("Введите ТРЕХзначное число: ");
+//     int num1 = Convert.ToInt32(Console.ReadLine());
+//     num = num1;
+// }
+// System.Console.WriteLine(num % 10);
+
+
+
+// Напишите программу, которая принимает на вход целое число любой разрядности и на выходе 
+// показывает третью цифру слева этого числа или говорит, что такой цифры нет. Через строку решать нельзя.
+// 456111 -> 6
+// 78 -> нет
+// 9146548 -> 4
+// 3 -> нет
+
+Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-while (true)
+if (num < 100)
 {
-    
+    System.Console.WriteLine("нет");
+}
+else
+{
+    while (num > 1000)
+    {
+        num = num / 10;
+    }
+    System.Console.WriteLine(num % 10);
 }
