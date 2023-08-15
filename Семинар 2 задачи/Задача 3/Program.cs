515f38e2a4a27eef316a842cd7e6b6f1f96bdcd0
Aleksandr <lexx-j@mail.ru>
2023-08-15 21:18:34 +0300

    программа для схемы из дз3 по контролю версий

--- Семинар 2 задачи/Задача 3/Program.cs
+++ Семинар 2 задачи/Задача 3/Program.cs
@@ -37,3 +37,71 @@ else
 {
     Console.WriteLine("нет");
 }
+
+
+
+
+
+
+
+// задача 1 необязательная
+
+// на входе целое или вещественное число, надо удалить вторую цифру слева этого числа.
+
+// 1,45 -> 1,5
+// 1 -> нет
+// 567,123 -> 57,123
+
+// Console.Write("Введите число: ");
+// decimal numD = Convert.ToDecimal(Console.ReadLine());
+// int numI = Convert.ToInt32(numD);
+// int i = 10;
+// int dotIndex = 10;
+// if (numD == numI)
+//     if (numD < 10)
+//         System.Console.WriteLine("нет");
+//     else
+//     {
+//         while (numI >= 10)
+//         {
+//             numI = numI / 10;
+//             i = i * 10;
+//             System.Console.WriteLine(numI);
+//             System.Console.WriteLine($"i={i}");
+//         }
+//         i = i / 100;
+//         System.Console.WriteLine(numI * i + Convert.ToInt32(numD) % i);
+//     }
+// else
+// {
+//     if (numI < 10)
+//     {
+//         dotIndex = dotIndex / 10;
+//         i = i * 10;
+//     }
+//     while (numD != numI)
+//     {
+//         numD = numD * 10;
+//         numI = Convert.ToInt32(numD);
+//         dotIndex = dotIndex * 10;
+//         System.Console.WriteLine($"numD = {numD}");
+//         System.Console.WriteLine($"numI = {numI}");
+//         System.Console.WriteLine($"dotIndex = {dotIndex}");
+//     }
+//     dotIndex = dotIndex / 10;
+//     while (numI >= 10)
+//     {
+//         numI = numI / 10;
+//         i = i * 10;
+//         System.Console.WriteLine(numI);
+//         System.Console.WriteLine($"i={i}");
+//     };
+//     i = i / 100;
+//     System.Console.WriteLine();
+//     System.Console.WriteLine($"numD = {numD}");
+//     System.Console.WriteLine($"numI = {numI}");
+//     System.Console.WriteLine($"dotIndex = {dotIndex}");
+//     System.Console.WriteLine($"i={i}");
+//     System.Console.WriteLine(Convert.ToDouble((numI * i + Convert.ToInt32(numD) % i)) / dotIndex);
+// }
+
